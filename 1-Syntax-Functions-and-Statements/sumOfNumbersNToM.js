080177a3bfe9eb5cda453295cef55c4dc7ed196d
Kiril Valkov <kiril_dgr@abv.bg>
2022-03-02 20:36:27 +0200

    Added sumOfNumbersNToM.js

---
+++ 1-Syntax-Functions-and-Statements/sumOfNumbersNToM.js
@@ -0,0 +1,13 @@
+function sumOfNumbersNToM(n, m) {
+    let num1 = Number(n);
+    let num2 = Number(m);
+    let result = 0;
+
+    for (let i = num1; i <= num2; i++) {
+        result += i;
+    }
+    return result;
+}
+
+sumOfNumbersNToM('1', '5');
+sumOfNumbersNToM('-8', '20');
