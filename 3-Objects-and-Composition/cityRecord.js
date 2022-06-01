c6258dc3e49b8875f04343c2b1556053ec55726b
Kiril Valkov <kiril_dgr@abv.bg>
2022-06-01 20:23:56 +0300

    Added cityRecord.js

---
+++ 3-Objects-and-Composition/cityRecord.js
@@ -0,0 +1,11 @@
+function cityTax(name, population, treasury) {
+    let result = {
+        name,
+        population,
+        treasury,
+    }
+    return result;
+}
+
+console.log(cityTax('Tortuga', 7000, 15000));
+console.log(cityTax('Santo Domingo', 12000, 23500));
