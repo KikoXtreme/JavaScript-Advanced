6eff7f340094e59e4d05cefbc538c894c149e085
Kiril Valkov <kiril_dgr@abv.bg>
2022-06-16 15:50:10 +0300

    Added constructionCrew.js

---
+++ 3-Objects-and-Composition/constructionCrew.js
@@ -0,0 +1,25 @@
+function constructionCrew(object) {
+
+    if (object.dizziness) {
+        object.levelOfHydrated += object.weight * object.experience * 0.1;
+        object.dizziness = false;
+    }
+    return object;
+}
+
+constructionCrew(
+    {
+        weight: 80,
+        experience: 1,
+        levelOfHydrated: 0,
+        dizziness: true
+    }
+);
+constructionCrew(
+    {
+        weight: 120,
+        experience: 20,
+        levelOfHydrated: 200,
+        dizziness: true
+    }
+);
