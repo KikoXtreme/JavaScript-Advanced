5bdfdd3ce15ed0d6f9ad6123c6e5f29e93506074
Kiril Valkov <kiril_dgr@abv.bg>
2022-06-26 09:42:24 +0300

    Added accordion.js

---
+++ 4-DOM-Introduction/2-Excercise/03. Accordion/accordion.js
@@ -0,0 +1,16 @@
+function toggle() {
+    let button = document.querySelector('.button');
+    let text = document.getElementById('extra');
+    // button.textContent = button.textContent === 'More' ? 'Less' : 'More';
+    if (button.textContent === 'More') {
+        button.textContent = 'Less';
+    } else {
+        button.textContent = 'More';
+    }
+
+    if (text.style.display === 'none' || text.style.display === '') {
+        text.style.display = 'block';
+    } else {
+        text.style.display = 'none';
+    }
+}
